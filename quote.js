cbb0a5821af0e90d127aaef90ebbb2feaa22368d
Charles <charlesfaye99@gmail.com>
2025-05-20 22:32:38 +0100

    Adding quote.js file

---
+++ quote.js
@@ -0,0 +1,13 @@
+
+export const quotes = [
+    {quote : "Rendre un salut n'a jamais écorché la bouche.", author: "Contes d'Amadou Koumba"},
+    {quote : "L'on ne connaît l'utilité des fesses que quand vient l'heure de s'asseoir.", author : "Contes d'Amadou Koumba"},
+    {quote : "Il y'a un Dieu qui façonne à son gré nos destinées qu'elle qu'en soit l'ébauche faite par nous.", author: "Shakespeare"},
+    {quote: "Rien n'est mauvais, rien n'est bon, c'est notre pensée qui crée le bonheur ou le malheur.", author: "Shakespeare"},
+    {quote: "Tout homme m'est supérieur en quelque manière et je m'instruis auprès de lui.", author: "Emerson"},
+    {quote: "Un homme grand montre sa grandeur dans la manière dont il traite les petits gens.", author: "Carlyle"},
+    {quote: "Il n'y a pas de spectacle plus désolant que celui d'un jeune pessimiste.", author: "Mark Twain"},
+    {quote: "Quand je travaille ça me repose, ne rien faire ou recevoir des visites me fatigue.", author: "Pablo Picasso"},
+    {quote: "Le secret du succès, c'est de faire coincider vocation et vacances.", author : "Mark Twain"},
+    {quote: "Ne cherchez pas la faute, cherchez le remède.", author: "Henry Ford"}
+]
